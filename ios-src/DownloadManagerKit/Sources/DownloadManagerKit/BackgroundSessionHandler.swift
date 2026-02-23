b66e0247319c7157ba95491e4fe191b54cc0a973
Matthew Richardson <matthew@velocityengineering.co.uk>
2026-02-23 15:58:15 +0000

    fix: dispatch completion on main thread

--- ios-src/DownloadManagerKit/Sources/DownloadManagerKit/BackgroundSessionHandler.swift
+++ ios-src/DownloadManagerKit/Sources/DownloadManagerKit/BackgroundSessionHandler.swift
@@ -3,6 +3,8 @@
 //  DownloadManagerKit
 //
 
+import Foundation
+
 /// Actor that synchronizes access to the background session completion handler.
 /// Handles the race condition where the URLSession delegate may fire before the handler is set.
 actor BackgroundSessionHandler {
@@ -13,15 +15,15 @@ actor BackgroundSessionHandler {
       completionHandler = handler
       if pendingComplete {
          pendingComplete = false
-         handler()
          completionHandler = nil
+         DispatchQueue.main.async { handler() }
       }
    }
    
    func handleComplete() {
       if let handler = completionHandler {
-         handler()
          completionHandler = nil
+         DispatchQueue.main.async { handler() }
       } else {
          pendingComplete = true
       }
