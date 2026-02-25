f7038f07477b5e8924e1adebc84cf2ab64ce3da7
Matthew Richardson <matthew@velocityengineering.co.uk>
2026-02-25 14:43:26 +0000

    test: add URL parser test and Kotlin runner

--- android/lib/build.gradle.kts
+++ android/lib/build.gradle.kts
@@ -39,4 +39,5 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
+    testImplementation("junit:junit:4.13.2")
 }
